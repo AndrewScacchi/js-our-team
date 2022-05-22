ac6eece4a5f345452bfb0cedc8bf78ca0c5b1ea5
andrea scacchi <andrea.scacchi@live.com>
2022-05-22 13:14:53 +0200

    Final Commit, completed basic exercise request

--- script.js
+++ script.js
@@ -32,38 +32,39 @@ const ourTeam = [
 
 
 ];
-console.log(ourTeam);
+// creating a cycle for
 
 
 // selecting the div Container
 const containerTeam = document.querySelector("div.team-container");
 
-// create card Element
-let teamCard = document.createElement("div");
-teamCard.classList.add("team-card");
-containerTeam.append(teamCard);
-// create div.card-image
-let divImage = document.createElement("div");
-divImage.classList.add("card-image");
-teamCard.append(divImage);
-    let img = document.createElement("img");
-    img.src = ourTeam[0].photo;
-    divImage.append(img);
+for (i = 0; i < ourTeam.length; i++){
+    // create card Element
+    let teamCard = document.createElement("div");
+    teamCard.classList.add("team-card");
+    containerTeam.append(teamCard);
+    // create div.card-image
+    let divImage = document.createElement("div");
+    divImage.classList.add("card-image");
+    teamCard.append(divImage);
+        let img = document.createElement("img");
+        img.src = ourTeam[i].photo;
+        divImage.append(img);
 
-// create div.card-text
-let divText = document.createElement("div");
-divText.classList.add("card-text");
-    let nameTeam = document.createElement("h3");
-    nameTeam.innerHTML = ourTeam[0].name;
-    divText.append(nameTeam);
+    // create div.card-text
+    let divText = document.createElement("div");
+    divText.classList.add("card-text");
+        let nameTeam = document.createElement("h3");
+        nameTeam.innerHTML = ourTeam[i].name;
+        divText.append(nameTeam);
 
-    let role = document.createElement("p");
-    role.innerHTML = ourTeam[0].role;
-    divText.append(role);
+        let role = document.createElement("p");
+        role.innerHTML = ourTeam[i].role;
+        divText.append(role);
 
-// final card assembly
-teamCard.append(divText);
-teamCard.append(divImg);
-containerTeam.append(teamCard);
+    // final card assembly
+    teamCard.append(divText);
+    teamCard.append(divImage);
+    containerTeam.append(teamCard);
 
-
+}
